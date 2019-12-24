e1f8dac14f5704782fca63dbb672f89df083aae8
Ilya Lebedev <bacherko@gmail.com>
2019-12-24 15:10:40 +0300

    feat: add fcm service

--- processor/data/env.js
+++ processor/data/env.js
@@ -5,4 +5,5 @@ module.exports = {
     GLS_PRISM_API_CONNECT: env.GLS_PRISM_API_CONNECT || env.GLS_PRISM_CONNECT,
     GLS_GUARANTEED_BLOCK_NUM: parseInt(env.GLS_GUARANTEED_BLOCK_NUM, 10) || null,
     GLS_MQ_CONNECT: env.GLS_MQ_CONNECT || 'amqp://localhost',
+    FIREBASE_AUTH_FILE: env.FIREBASE_AUTH_FILE || './auth-firebase-adminsdk.json',
 };
