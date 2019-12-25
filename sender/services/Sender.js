6e6dedefc699bf5a7284cd3079c070ced2b5988f
Ilya Lebedev <bacherko@gmail.com>
2019-12-25 14:31:18 +0300

    feat: add fcm service (#4)

--- sender/services/Sender.js
+++ sender/services/Sender.js
@@ -1,4 +1,6 @@
+const fs = require('fs');
 const mq = require('amqplib');
+const fcm = require('firebase-admin');
 const core = require('cyberway-core-service');
 const { Service } = core.services;
 const { Logger } = core.utils;
@@ -8,12 +10,19 @@ const { getConnector } = require('../utils/processStore');
 
 const QUEUE_NAME = 'notifications';
 
+const fcmAuth = JSON.parse(fs.readFileSync(env.FIREBASE_AUTH_FILE));
+
 class Sender extends Service {
     constructor() {
         super();
 
         this._mq = null;
         this._channel = null;
+
+        fcm.initializeApp({
+            credential: fcm.credential.cert(fcmAuth),
+            databaseURL: `https://${fcmAuth.project_id}.firebaseio.com`,
+        });
     }
 
     async start() {
@@ -45,15 +54,61 @@ class Sender extends Service {
             userId,
         });
 
-        console.log('tokens:', tokens);
-
         if (!tokens.length) {
-            // return;
+            return;
         }
 
         const notification = await con.callService('notifications', 'getNotification', { id });
 
-        console.log('notification =', notification);
+        await this._sendPush(notification, tokens.map(({ fcmToken }) => fcmToken));
+    }
+
+    async _sendPush(notification, tokens) {
+        const message = {
+            tokens,
+            data: notification,
+            notification: {
+                body: this._extractBody(notification),
+            },
+        };
+
+        Logger.info('Try to send notification:', message);
+
+        try {
+            const response = await fcm.messaging().sendMulticast(message);
+            Logger.info('FCM response:', response);
+        } catch (err) {
+            Logger.warn('Error sending message:', err);
+        }
+    }
+
+    _extractBody(notification) {
+        const entry = notification.comment || notification.post || null;
+
+        switch (notification.eventType) {
+            case 'upvote': {
+                let text = `${notification.voter.username} liked your ${notification.entityType}`;
+
+                if (entry.shortText) {
+                    text += `: “${entry.shortText}”`;
+                }
+
+                return text;
+            }
+
+            case 'mention':
+                return `${notification.author.username} mentioned you in a ${notification.entityType}: “${entry.shortText}”`;
+
+            case 'subscribe':
+                return `${notification.use.username} is following you`;
+
+            default:
+                Logger.error(
+                    `Unknown notification type (${notification.eventType}):`,
+                    notification
+                );
+                throw new Error('Invalid event type');
+        }
     }
 }
 
